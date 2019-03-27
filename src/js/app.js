908dc6bba5cc386ffdd79858a6743199fe04fb26
fpiaggio <fpiaggio92@gmail.com>
2019-03-27 17:51:56 -0300

    Directiva que recibe eventos y si no tienen 'start' y 'end' los refactorea tomándolos de las props 'startAt' y 'endAt'.
    Falta formatear las fechas correctamente.

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,27 @@
-const mainController = ($scope) => {
+const mainController = ($scope, $http) => {
     const styles = `
         color: white; 
-        background: red; 
+        background: peru; 
         font-size: 26px;
         font-weight: bold;
         padding: 1em;
         margin: 1em 0;
+        border-radius: 5px;
     `;
-    console.info('%cAguante Angular 1', styles);
+    console.info('%c(╯°□°）╯︵ ┻━┻', styles);
+    $scope.saludo = (ev) => console.log('Hiciste click en: ', ev.el.innerText);
+    $scope.exampleEvents = [
+        {
+            id: '1',
+            title: 'Evento de prueba',
+            start: '2019-04-01'
+        }
+    ];
+    $scope.apiEvents = [];
+    $http.get('./src/api/events.json').then(function(res){
+        $scope.apiEvents = res.data;
+    });
 };
 
 const app = angular.module('app', []).controller('mainController', mainController);
-mainController.$inject = ['$scope'];
+mainController.$inject = ['$scope', '$http'];
--- src/js/directives/debCalendar.js
+++ src/js/directives/debCalendar.js
@@ -1,15 +1,75 @@
 var debCalendarDirective = function(){
     function link (scope, element){
+
+        /**
+         * Recibe una lista de eventos sin las key 'start' y 'end'
+         * y se las agrega copiando los valores de 'startAt' y 'endAt'
+         * @param events [array]
+         * @returns {object}
+         */
+        var refactorEvents = function(events){
+            // Si no hago un angular copy corre el watcher 9999 veces
+            // Eso rompe angular  ¯\_(ツ)_/¯
+            var refactored = angular.copy(events);
+            refactored.map(function(event){
+                event.start = event.startAt.substring(event.startAt.indexOf('T'), 0);
+                event.end = event.endAt.substring(event.startAt.indexOf('T'), 0);
+                event.title = event.code;
+                return event;
+            });
+            return refactored;
+        };
+
+        // Check default props values
+        if(!scope.clickEventCb){
+            scope.clickEventCb = function(){ return false; }
+        }
+
+        // DOM element
         var domCalendar = element[0].querySelectorAll('#calendar')[0];
-        var calendar = new FullCalendar.Calendar(domCalendar, {
-            plugins: [ 'dayGrid' ]
-        });
+
+        // Calendar config
+        var configCalendar = {
+            locale: "es",
+            plugins: [ 'interaction', 'dayGrid', 'timeGrid' ],
+            timeZone: 'UTC',
+            defaultView: 'dayGridMonth',
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            },
+            events: scope.events,
+            eventClick: scope.clickcb
+        };
+
+        // Calendar render
+        var calendar = new FullCalendar.Calendar(domCalendar, configCalendar);
         calendar.render();
+
+        // Watcher + re-render
+        scope.$watch('events', function(){
+            var ev = scope.events;
+            // Necesito la prop start, si no la tiene hago un refactor y se lo agrego
+            if(ev[0] && ev[0].hasOwnProperty('startAt') && !ev[0].hasOwnProperty('start')){
+                scope.events = refactorEvents(scope.events);
+            }
+            // Actualizo la config y hago el re-render
+            configCalendar.events = scope.events;
+            calendar.destroy();
+            calendar = new FullCalendar.Calendar(domCalendar, configCalendar);
+            calendar.render();
+        });
+
     }
     return {
-      restrict: 'E',
-      templateUrl: 'src/templates/debCalendar.html',
-      link: link
+        restrict: 'E',
+        templateUrl: 'src/templates/debCalendar.html',
+        scope: {
+            clickcb: '=',
+            events: '='
+        },
+        link: link
     }
 };
 app.directive('debCalendar', debCalendarDirective);
